8d71324d258ff9624659f24ae7df2ad6d5658cde
Kapil Garg <105214446+kappil-garg@users.noreply.github.com>
2025-07-08 19:27:13 +0530

    Add demos for Java basics
    - DifferentClassName.java to demonstrate that class name can differ from the file name.
    - MultipleClassesDemo.java to show multiple classes in one file.
    - CastingDemo.java for Type casting demo (implicit & explicit).

---
+++ src/com/kapil/masteringjava/basics/DifferentClassNameDemo.java
@@ -0,0 +1,16 @@
+package com.kapil.masteringjava.basics;
+
+/**
+ * Program to demonstrate source file name can differ from the class name in Java, except when the class is public.
+ * If the class is declared public, the file name must match the class name exactly.
+ *
+ * @author Kapil Garg
+ */
+class DifferentClassName {
+
+    public static void main(String[] args) {
+        System.out.println("\nHello from Class :: DifferentClassName");
+        System.out.println("Source File Name :: DifferentClassNameDemo.java");
+    }
+
+}
