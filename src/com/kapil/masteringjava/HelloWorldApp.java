256b6dd55eb7d6085946e2c1da47f664045c8fda
Kapil Garg <105214446+kappil-garg@users.noreply.github.com>
2025-09-05 00:25:23 +0530

    Add first program

---
+++ src/com/kapil/masteringjava/HelloWorldApp.java
@@ -0,0 +1,17 @@
+package com.kapil.masteringjava;
+
+/**
+ * Program to demonstrate a simple "Hello, World!" application in Java.
+ * Demonstrates the use of System.out.print and System.out.println methods.
+ *
+ * @author Kapil Garg
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.print("\nHello, World!\n");                              // \n for the new line
+        System.out.println("I'm Kapil Garg, ready to explore Java.");       // println automatically adds a new line
+        System.out.println("Welcome to the world of Java programming.");
+    }
+
+}
