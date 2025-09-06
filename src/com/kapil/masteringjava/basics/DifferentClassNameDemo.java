9413271dc56c4e414ab760c2cf31f998a9ace60f
Kapil Garg <105214446+kappil-garg@users.noreply.github.com>
2025-09-06 22:43:32 +0530

    Add demos for basics
    Includes demos covering:
    - File and class naming rules when a class is not public
    - Defining multiple classes in a single Java source file
    - Performing implicit and explicit type casting between primitive types

---
+++ src/com/kapil/masteringjava/basics/DifferentClassNameDemo.java
@@ -0,0 +1,16 @@
+package com.kapil.masteringjava.basics;
+
+/**
+ * Program to demonstrate that the source file name can differ from the class name in Java.
+ * Demonstrates that if the class is not public, the file name can be different.
+ *
+ * @author Kapil Garg
+ */
+class DifferentClassName {
+
+    public static void main(String[] args) {
+        System.out.println("\nHello from Class :: DifferentClassName");
+        System.out.println("Source File Name :: DifferentClassNameDemo.java");
+    }
+
+}
